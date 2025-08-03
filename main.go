7fcfa71c69d9ce8f79cee7741a7ef71a044b770f
Josh Potts <9337140+jlpdeveloper@users.noreply.github.com>
2025-08-02 19:50:04 -0500

    Replace `log` with `slog` for structured logging and integrate JSON-based logger

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"chi-boilerplate/router"
 	"log"
+	"log/slog"
 	"net/http"
 	"os"
 )
 
 func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
 	r := router.SetupRouter()
 	port := getEnvVarValue("PORT", "8080")
 	if err := http.ListenAndServe(":"+port, r); err != nil {
@@ -18,7 +21,7 @@ func main() {
 func getEnvVarValue(key string, defaultValue string) string {
 	val, found := os.LookupEnv(key)
 	if !found {
-		log.Println("Environment variable " + key + " not found")
+		slog.Info("Environment variable " + key + " not found")
 		return defaultValue
 	}
 	return val
