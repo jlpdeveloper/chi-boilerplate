816b75d02467c96c801af8cc1b16781c2eb7426e
Josh Potts <9337140+jlpdeveloper@users.noreply.github.com>
2025-08-05 19:33:04 -0500

    Gracefully handle server shutdown logic with timeout using context and signal notifications.

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"chi-boilerplate/router"
+	"context"
+	"errors"
 	"log"
 	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -13,9 +18,26 @@ func main() {
 	slog.SetDefault(logger)
 	r := router.SetupRouter()
 	port := getEnvVarValue("PORT", "8080")
-	if err := http.ListenAndServe(":"+port, r); err != nil {
-		log.Fatalf("Failed to start server: %v", err)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: r,
 	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGQUIT, syscall.SIGTERM)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+
 }
 
 func getEnvVarValue(key string, defaultValue string) string {
